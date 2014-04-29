b0e23708ad068e6a230a6f2ba3d9580a6925a3f2
psd <psd@whatfettle.com>
2014-04-29 12:17:54 +0100

    Added model for a title

--- concept_official_copy/model/title.py
+++ concept_official_copy/model/title.py
@@ -3,6 +3,7 @@ A module containing the :py:class:`Title` class
 """
 
 import datetime
+import jsonpickle
 
 def current_timestring():
     return datetime.datetime.now().isoformat()
@@ -12,12 +13,12 @@ class Title(object):
     The primary content object in the Land Registry
     """
 
-    def __init__(self, title_number=None):
-        """
-        Create a new Title object.
-
-        A ``title_number`` is required to create a new title.
-        """
-        self.title_number = title_number
-        self.created = u''
+    def __init__(self, title_number=None, data=None):
+        if data:
+            self.__dict__ = data
+        if title_number:
+            self.title_number = title_number
         self.accessed = current_timestring()
+
+    def as_json(self):
+        return jsonpickle.encode(self)
--- test/model_title_test.py
+++ test/model_title_test.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 
 import os
+import re
 from concept_official_copy.model.title import Title
-import unittest
-import mock
+import pytest
 
-class TestModelTitle(unittest.TestCase):
-    def test_init(self):
-        t = Title('EXAMPLE')
-        assert t.title_number == 'EXAMPLE'
+re_isodatetime = re.compile("^20\d{2}-[0-1]\d-[0-3]\dT[0-2]\d:\d{2}:\d{2}.\d{2,}$")
 
-if __name__ == '__main__':
-    unittest.main()
+def test_create_title():
+    title = Title('EXAMPLE')
+
+    assert type(title) == Title, ('Title returns a Title, %s, %s' % (type(title), Title))
+    assert '<concept_official_copy.model.title.Title object' in '%s' % title
+
+    assert title.title_number == 'EXAMPLE'
+    assert re_isodatetime.match(title.accessed) is not None
